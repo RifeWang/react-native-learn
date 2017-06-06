be9e2a22ac3f0ebb8ff48a7f4b9234e9b4596555
RifeWang <rifewang@gmail.com>
2017-06-06 14:53:14 +0800

    .

--- index.android.js
+++ index.android.js
@@ -1,53 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  WebView
 } from 'react-native';
+import { TabBar, SearchBar } from 'antd-mobile';
+// import Swiper from 'react-native-swiper';//轮播组件,暂时有问题
+import Myswiper from './components/carousel'
 
-export default class ReactNativeHello extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+export default class Footer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedTab: 'redTab',
+        }
+    }
+
+    renderContend(pageText) {
+        return (
+            <View style={{ flex:1, alignItems:'center', backgroundColor:'white'}}>
+                <Text style={{ margin: 50 }}>{pageText}</Text>
+            </View>
+        );
+    }
+
+    onChangeTab(tabName) {
+        this.setState({
+            selectedTab: tabName,
+        });
+    }
+
+    webviewTest(){
+        return (
+            <WebView
+                source={require('./public/gold-price365.html')}
+                domStorageEnabled={true} //android
+                javaScriptEnabled={true} //android
+            />
+        );
+    }
+
+    render(){
+        return (
+            <View style={{flex:1,justifyContent:'space-between'}}>
+                {/* <View> */}
+                    <Myswiper/>
+                {/* </View> */}
+                <View style={{flex:1}}>
+                <TabBar
+                    unselectedTintColor='#949494'
+                    tintColor="#33A3F4"
+                    barTintColor="#ccc"
+                >
+                    <TabBar.Item
+                        title='生活'
+                        selected={this.state.selectedTab === 'blueTab'}
+                        onPress={() => this.onChangeTab('blueTab')}
+                        style={{flexDirection: 'row',alignItems: 'flex-start'}}
+                    >
+                        {this.webviewTest()}
+                    </TabBar.Item>
+
+                    <TabBar.Item
+                        title='口碑'
+                        selected={this.state.selectedTab === 'redTab'}
+                        onPress={() => this.onChangeTab('redTab')}
+                    >
+                        {this.renderContend('口碑 Tab')}
+                    </TabBar.Item>
+
+                    <TabBar.Item
+                        title='朋友'
+                        selected={this.state.selectedTab === 'greenTab'}
+                        onPress={() => this.onChangeTab('greenTab')}
+                    >
+                        {this.renderContend('朋友 Tab')}
+                    </TabBar.Item>
+
+                    <TabBar.Item
+                        title='我的'
+                        badge={2}
+                        selected={this.state.selectedTab === 'yellowTab'}
+                        onPress={() => this.onChangeTab('yellowTab')}
+                    >
+                        {this.renderContend('我的 Tab')}
+                    </TabBar.Item>
+
+                </TabBar>
+                </View>
+            </View>
+
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('ReactNativeHello', () => ReactNativeHello);
+AppRegistry.registerComponent('ReactNativeHello', () => Footer);
